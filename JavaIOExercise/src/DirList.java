2a4fe24b0bce7dd237b012b654bd4ce6dca93386
zhuxinquan <1174272148@qq.com>
2016-01-13 21:57:57 +0800

    列出文件夹中所有匹配要求的文件

--- JavaIOExercise/src/DirList.java
+++ JavaIOExercise/src/DirList.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 
 /**
  * Created by zhuxinquan on 16-1-13.
+ * 输出目录中匹配的文件
  */
 public class DirList {
     public static void main(String[] args) {
@@ -13,8 +14,10 @@ public class DirList {
         if(args.length == 0){
             list = path.list();
         }else{
+            //DirFilter类实现了FilenameFilter中的方法，用来进行过滤
             list = path.list(new DirFilter(args[0]));
         }
+        //String.CASE_INSENSITIVE_ORDER为不区分大小写对字符串进行排序
         Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
         for(String dirItem : list){
             System.out.println(dirItem);
@@ -25,9 +28,11 @@ public class DirList {
 class DirFilter implements FilenameFilter{
     private Pattern pattern;
     public DirFilter(String regex){
+        //将给定的正则表达式编译并赋予给Pattern类
         pattern = Pattern.compile(regex);
     }
     public boolean accept(File dir, String name){
+        //matcher生成一个给定命名的Matcher对象,matches对输入的字串以该正则表达式为模开展匹配
         return pattern.matcher(name).matches();
     }
 }
