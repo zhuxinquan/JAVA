fb3e8937934bd3ee3e709bc72228d2bc5076b119
zhuxinquan <1174272148@qq.com>
2016-01-24 20:10:27 +0800

    使用Dom解析xml文件

--- XMLExercise/src/xml/DomParseDemo.java
+++ XMLExercise/src/xml/DomParseDemo.java
@@ -60,9 +60,9 @@ public class DomParseDemo {
                 }else if("desc".equals(tagName)){
                     //获取teacher节点下的desc值
                     t.setDesc(childNode.getFirstChild().getNodeValue());
-                }/*else{
-                    System.out.println(childNode.getFirstChild().getNodeValue());
-                }*/
+                }else{
+                    System.out.println(tagName);
+                }
             }
             teachers.add(t);
         }
