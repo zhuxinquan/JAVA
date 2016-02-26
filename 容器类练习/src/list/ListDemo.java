86f09eb917281f643de0b7d1f7ec7186447593c6
zhuxinquan <1174272148@qq.com>
2016-02-26 20:12:07 +0800

    List

--- 容器类练习/src/list/ListDemo.java
+++ 容器类练习/src/list/ListDemo.java
@@ -1,6 +1,7 @@
 package list;
 
 import java.util.ArrayList;
+import java.util.Vector;
 
 /**
  * List接口 允许重复值 空值  必须有序
@@ -8,21 +9,28 @@ import java.util.ArrayList;
  */
 public class ListDemo {
 
+    public static void vector(){
+        Vector vector = new Vector();
+        vector.add("test");
+        System.out.println(vector);
+    }
+
     /**
      * ArrayList实现类
      */
     public static void arrayList(){
-        ArrayList list = new ArrayList();
-        list.add(1);
-        list.add("+");
-        list.add(1);
-        list.add("=");
-        list.add(2);
-        list.add(3, "123");
-
-        System.out.println(list);
-        System.out.println(list.get(3));
-        System.out.println(list.indexOf(1));
+        vector();
+//        ArrayList list = new ArrayList();
+//        list.add(1);
+//        list.add("+");
+//        list.add(1);
+//        list.add("=");
+//        list.add(2);
+//        list.add(3, "123");
+//
+//        System.out.println(list);
+//        System.out.println(list.get(3));
+//        System.out.println(list.indexOf(1));
     }
 
     public static void main(String[] args) {
