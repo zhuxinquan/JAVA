bd3d06256ab42cc422b5c08f83482004887e4118
zhuxinquan <1174272148@qq.com>
2016-06-15 22:51:30 +0800

    update

--- JAVA上机/src/实验六_IO流/CopyFile.java
+++ JAVA上机/src/实验六_IO流/CopyFile.java
@@ -1,10 +1,13 @@
 package 实验六_IO流;
 
 import java.io.*;
+
 public class CopyFile {
     public static void main(String[] args) {
         try {
             FileInputStream fis = new FileInputStream("./src/实验六_IO流/CopyFile1.java");
+//            Scanner in = new Scanner(System.in);
+//            String filename = in.next();
             FileOutputStream fos = new FileOutputStream("temp.txt");
             int read = fis.read();
             while ( read != -1 ) {
